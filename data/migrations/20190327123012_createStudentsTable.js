0365d6322426289b312de17d03970288b3fff364
mark-marshall <mark.marshallgp@gmail.com>
2019-03-27 12:33:31 +0000

    [MVP] Added tables for cohorts and students

---
+++ data/migrations/20190327123012_createStudentsTable.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('students', table => {
+      table.increments();
+
+      table.string('name', 128)
+
+      table.integer('cohort_id')
+      .unsigned()
+      .references('id')
+      .inTable('cohorts')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  
+};
